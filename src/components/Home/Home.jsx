87e4bd0d68df9574b63b58a7b2ecf5c405c8d45e
Anny Pereira <annypereiras@gmail.com>
2022-07-19 14:12:03 +0200

    first changes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Home.css";
+
+
+
+// const NavBar = (props) => {
+//     return (
+//     <div>
+//         <ul>
+//             <li>
+//                 <a href="#home">Inicio</a>
+//             </li>
+//             <li>
+//                 <a href="#aboutMe">Sobre mi</a>
+//             </li>
+//             <li>
+//                 <a href="#experience_education">Experiencia y estudios</a>
+//             </li>
+//             <li>
+//                 <a href="#contactMe">Contáctame</a>
+//             </li>
+//         </ul>
+//     </div>)
+    
+// };
+
+const Home = ({ home }) => {
+    <div> 
+        <img src="" alt=""/>
+        <h3>Anny Pereira Sánchez</h3>
+    </div>
+};
+
+export default Home;
---
+++ src/cv/CV.js
@@ -0,0 +1,87 @@
+export const CV = {
+   home: {
+    name: "Anny",
+    lastname: "Pereira Sánchez",
+    adress: "Barcelona",
+    zipCode: "08041",
+    mail: "annypereiras@gmail.com",
+    phone: "605728054",
+    gitHub: "https://github.com/annypereira",
+    linkedin: "https://www.linkedin.com/in/anny-pereira/"
+   },
+
+   aboutMe: [
+    "Soy una persona organizada, con experiencia liderando y gestionando equipos de trabajo.",
+    "Me gusta la música y me encanta hacer tartas y postres.",
+    "Tengo mucha motivación y ganas de seguir aprendiendo y creciendo profesionalmente.",
+    "Además de las skills mencionadas anteriormente he aprendido sobre HTML, CSS, JavaScript, TypeScript, Angular y React"
+   ],
+
+   education: [
+    {
+        name: "Front-End Developer",
+        date: "Julio de 2022",
+        where: "Upgrade Hub Madrid"
+    },
+    {
+        name: "Introducción a la programación web",
+        date: "Diciembre de 2021",
+        where: "CIFO L'Hospitalet - Barcelona "
+    },
+    {
+        name: "Licenciatura en Biología",
+        date: "Noviembre de 2016",
+        where: "Universidad de Carabobo - Venezuela"
+    }
+   ],
+
+   experience: [
+    {
+        name: "Store Manager",
+        date: "Septiembre de 2018 - Diciembre de 2021",
+        where: "La Casa De Las Carcasas",
+        description: "Gestión de equipo, control de stock, "
+    },
+    {
+        name: "Dependienta",
+        date: "Febrero de 2018 - Agosto de 2018",
+        where: "Padedebo",
+        description: ""
+    },
+    {
+        name: "CEO - Pastelera",
+        date: "Agosto de 2013 - Febrero de 2017",
+        where: "Anny's Bakery",
+        description: ""
+    },
+    {
+        name: "Asesor de ventas",
+        date: "Noviembre de 2011 - Julio de 2013",
+        where: "Jumima 11",
+        description: ""
+    },
+   ],
+
+   languages: [
+    {
+        languages: "Castellano",
+        wrLevel: "Nativo",
+        spLevel: "Nativo"
+    },
+    {
+        languages: "Inglés",
+        wrLevel: "Avanzado",
+        spLevel: "Intermedio"
+    }
+   ], 
+
+   skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "Angular",
+    "React",
+    "GIT"
+   ]
+};
